b815f2bd4106ea4165a3075ae04c3614b5d22fa5
Matteo Paonessa <ardell.alarion@gmail.com>
2017-11-15 10:35:34 +0100

    Speed up PNG

--- caesium/caesium.c
+++ caesium/caesium.c
@@ -49,8 +49,8 @@ void initialize_jpeg_parameters(cs_image_pars *options)
 
 void initialize_png_parameters(cs_image_pars *par)
 {
-	par->png.iterations = 10;
-	par->png.iterations_large = 5;
+	par->png.iterations = 2;
+	par->png.iterations_large = 2;
 	par->png.block_split_strategy = 4;
 	par->png.lossy_8 = true;
 	par->png.transparent = true;
