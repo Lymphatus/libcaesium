4ac62810d18e436aec3b2e64cb814c4c3bac37d8
Matteo Paonessa <matteo.paonessa@gmail.com>
2019-11-02 15:08:26 +0100

    install.sh will use tagged mozjpeg/zopfli versions

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@
 SOURCE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #mozjpeg
-git clone https://github.com/mozilla/mozjpeg
-cd mozjpeg/
-mkdir build && cd build
+git clone --branch 'v3.3.1' --depth 1 https://github.com/mozilla/mozjpeg.git
+cd mozjpeg/ || exit
+mkdir build && cd build || exit
 cmake -G"Unix Makefiles" ..
 make && sudo make install
 
-cd ${SOURCE}
+cd "${SOURCE}" || exit
 
 #zopflipng
-git clone https://github.com/google/zopfli.git
-cd zopfli
+git clone --branch 'zopfli-1.0.2' --depth 1 https://github.com/google/zopfli.git
+cd zopfli || exit
 make libzopflipng
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sudo cp libzopflipng.so.1.0.2 /usr/local/lib
@@ -29,4 +29,4 @@ else
     sudo cp src/zopflipng/zopflipng_lib.h /usr/include/zopflipng
 fi
 
-cd ${SOURCE}
+cd "${SOURCE}" || exit
